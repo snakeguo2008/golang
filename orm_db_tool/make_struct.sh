d37a13e6f99225e4fe672b79450df3f508e80762
guoyunlong <sanguobaye1@126.com>
2019-10-30 16:53:49 +0800

    优化生成逻辑,支持数据库中所有表和指定前缀的表数据生成

--- orm_db_tool/make_struct.sh
+++ orm_db_tool/make_struct.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-#./table2struct --db_host 192.168.1.222 --db_port 3306 --db_pwd yichat --db_user yichat --db_name=yichat  --output ./outputdir
-./table2struct --db_host 192.168.1.222 --db_port 3306 --db_pwd yichat --db_user yichat --db_name=yichat  --output ./outputdir  --skip_if_no_prefix --table_prefix $1  --tag_gorm
+#./table2struct --db_host 192.168.1.222 --db_port 3306 --db_pwd yichat --db_user yichat --db_name=yichat  --output ./outputdiri
+OUTPUTDIR=outputdir
+if [ ! -d ${OUTPUTDIR} ]
+then
+	mkdir -p outputdir
+	echo "create dir outputdir"
+fi
+
+if [ $# -eq 0 ]
+then
+	echo "生成数据库全部的表映射"
+	./table2struct --db_host 192.168.1.222 --db_port 3306 --db_pwd yichat --db_user yichat --db_name=yichat  --output ./outputdir
+else
+	echo "生成数据库表中前缀为 $1 的表映射"
+	./table2struct --db_host 192.168.1.222 --db_port 3306 --db_pwd yichat --db_user yichat --db_name=yichat  --output ./outputdir  --skip_if_no_prefix --table_prefix $1  --tag_gorm
+fi
+
+
+
+#./table2struct --db_host 192.168.1.222 --db_port 3306 --db_pwd yichat --db_user yichat --db_name=yichat  --output ./outputdir  --skip_if_no_prefix --table_prefix $1  --tag_gorm
